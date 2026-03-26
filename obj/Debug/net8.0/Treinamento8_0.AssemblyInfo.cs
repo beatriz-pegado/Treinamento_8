f647a552b1f9a7359f78634f76c97a0c397644f3
beatriz.pegado <anabeatrizdasilvapegado@gmail.com>
2026-03-26 15:44:21 -0300

    Adiciona Pagina e Modulo

--- obj/Debug/net8.0/Treinamento8_0.AssemblyInfo.cs
+++ obj/Debug/net8.0/Treinamento8_0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Treinamento8_0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e619905ec819c96a40f15c5ae1f45461a5ad289")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07af72fe43ac63078bee0453f6f059143d77823")]
 [assembly: System.Reflection.AssemblyProductAttribute("Treinamento8_0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Treinamento8_0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
